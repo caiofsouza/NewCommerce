f846ab977cfc91e32da3b82592775f7c45ffd1cf
Caio Fernandes <caio_fsouza@hotmail.com>
2016-05-17 01:54:11 -0300

    fix

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ http.createServer(function(request, response) {
 				return;
 			}
 
-			response.writeHead(200, {'Content-Type': 'text/plain'});
+			response.writeHead(200, {'Content-Type': 'text/html'});
 			response.write(file);
 			response.end();
 		});
